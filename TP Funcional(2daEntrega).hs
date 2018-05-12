6db0c74c380beb5627714f1b13eed3f7490dc5e2
Matias Diaz <matiasdiaz142@gmail.com>
2018-05-11 23:52:23 -0300

    Update TP Funcional(2daEntrega).hs

--- TP Funcional(2daEntrega).hs
+++ TP Funcional(2daEntrega).hs
@@ -10,14 +10,14 @@ data Micro = Micro{
 	acumuladorB :: Int,
 	programCounter :: Int,
 	mensajeError :: String,
-	programas :: [Instruccion]
+	programa :: [Instruccion]
 }deriving (Show)
 type Instruccion = Micro -> Micro
 
 -- Punto 2: Se probo haciendo (nop.nop.nop)xt8088, se uso composicion
-xt8088 = Micro {memoria = (replicate 1024 0),acumuladorA = 0,acumuladorB = 0,programCounter = 0,mensajeError = "",programas=[swap.divide.(lod 1).swap.(lod 2).(str 2 0).(str 1 2)]} 
-fp20   = Micro {memoria = [],acumuladorA = 7,acumuladorB = 24,programCounter = 0,mensajeError = "",programas=[]}
-at8086 = Micro {memoria = [1..20],acumuladorA = 0,acumuladorB = 0,programCounter = 0,mensajeError = "",programas=[]}
+xt8088 = Micro {memoria = (replicate 1024 0),acumuladorA = 0,acumuladorB = 0,programCounter = 0,mensajeError = "",programa=[swap.divide.(lod 1).swap.(lod 2).(str 2 0).(str 1 2)]} 
+fp20   = Micro {memoria = [],acumuladorA = 7,acumuladorB = 24,programCounter = 0,mensajeError = "",programa=suma10y22}
+at8086 = Micro {memoria = [1..20],acumuladorA = 0,acumuladorB = 0,programCounter = 0,mensajeError = "",programa=[]}
 
 nop :: Micro -> Micro
 nop micro 
@@ -26,47 +26,41 @@ nop micro
  
 --Punto 3: Se probo haciendo (add.(lodv 22).swap.(lodv 10))fp20 se uso composicion
 lodv :: Int -> Micro -> Micro
-lodv val micro 
-	|hayError micro = micro
-	|otherwise = nop micro{acumuladorA = val}
+lodv val micro = nop micro{acumuladorA = val}
 
 swap :: Micro -> Micro 
-swap micro 
-	|hayError micro = micro
-	|otherwise = nop micro{acumuladorA = acumuladorB micro, acumuladorB = acumuladorA micro}
+swap micro = nop micro{acumuladorA = acumuladorB micro, acumuladorB = acumuladorA micro}
 
 add :: Micro -> Micro
-add micro 
-	|hayError micro = micro
-	|otherwise = nop micro{acumuladorA = acumuladorA micro + acumuladorB micro, acumuladorB = 0}
+add micro = nop micro{acumuladorA = acumuladorA micro + acumuladorB micro, acumuladorB = 0}
 
 --Punto 4  Se probo haciendo (divide.(lod 1).swap.(lod 2).(str 2 0).(str 1 2)) xt8088 
 
 divide :: Micro -> Micro
 divide micro  
-	|hayError micro = micro
 	|acumuladorB micro == 0 = nop micro{mensajeError = "Division By Zero"}
 	|otherwise = nop micro{acumuladorA = div (acumuladorA micro)(acumuladorB micro), acumuladorB = 0}
 		
 str :: Int -> Int -> Micro -> Micro
-str addr val micro 
-	|hayError micro = micro
-	|otherwise = nop micro{memoria = take (addr - 1) (memoria micro) ++ [val] ++ drop addr(memoria micro)}
+str addr val micro = nop micro{memoria = take (addr - 1) (memoria micro) ++ [val] ++ drop addr(memoria micro)}
 
 lod :: Int -> Micro -> Micro
-lod addr micro 
-	|hayError micro = micro
-	|otherwise = nop micro{acumuladorA = (!!)(memoria micro) (addr - 1)}
+lod addr micro = nop micro{acumuladorA = (!!)(memoria micro) (addr - 1)}
 
 --Segunda Entrega
 
+suma10y22 = [add,(lodv 22),swap,(lodv 10)]
+division2Por0 = [swap,swap,swap,divide,(lod 1),swap,(lod 2),(str 2 0),(str 1 2)]
+
 --Punto 1: Carga de un programa
-cargarPrograma :: Instruccion -> Micro -> Micro
-cargarPrograma unPrograma micro = micro{programas = programas micro ++ [unPrograma]}
+cargarPrograma :: [Instruccion] -> Micro -> Micro
+cargarPrograma unPrograma micro = micro{programa = programa micro ++ unPrograma}
 
 --Punto 2: Ejecución de un programa
 ejecutar :: Micro -> Micro
-ejecutar micro = foldr ($) micro (programas micro)
+ejecutar micro = foldr ($) micro (programa micro)
+--Hacer una funcion auxiliar para ****foldr ($) micro (programa micro)****
+--y usarla tambien en ifnz 
 
 hayError::Micro->Bool
 hayError = ((>0).length.mensajeError)
@@ -79,11 +73,11 @@ ifnz listaInstrucciones micro
 
 --Punto 4: Depuración de un programa
 depurar :: [Instruccion] -> Micro -> [Instruccion]
-depurar listaInstrucciones micro = filter (\f -> criterio(f micro)) listaInstrucciones
+depurar listaInstrucciones micro = filter (\f -> acumuladoresYMemoriaEnCero (f micro)) listaInstrucciones
 --depurar (swap.nop.(lodv 133).(lodv 0).(str 1 3).(str 2 0)) xt8088
 
-criterio :: Micro -> Bool 
-criterio micro = (acumuladorA micro /= 0) || (acumuladorB micro /= 0) || (sum (memoria micro) /= 0)
+acumuladoresYMemoriaEnCero :: Micro -> Bool 
+acumuladoresYMemoriaEnCero micro = (acumuladorA micro /= 0) || (acumuladorB micro /= 0) || (sum (memoria micro) /= 0)
 --all (/=0) (memoria micro)
 
 --Punto 5: Memoria ordenada
@@ -93,8 +87,16 @@ memoriaOrdenada [x,y] = x <= y
 memoriaOrdenada (x:y:xs) 
 	| x <= y = memoriaOrdenada (y:xs)
 	| otherwise = False
-	
+--Evaluar el caso de si se tiene un solo elemento o el conjunto vacio
+
 --Punto 6: Memoria infinita
-xtInfinito = Micro {memoria = [0,0..],acumuladorA = 0,acumuladorB = 0,programCounter = 0,mensajeError = "",programas=[]}
---Se queda cargando infinitamente la memoria, y no se llega a ver todo el Micro
---No llega a tirar False ni True,ya que, se queda analizando infinitamente a la memoria del Micro
+xtInfinito = Micro {memoria = [0,0..],acumuladorA = 0,acumuladorB = 0,programCounter = 0,mensajeError = "",programa=[]}
+--La operacion se completo pero se queda cargando infinitamente la memoria en la consola, pero aplicando:
+--en consola: (acumuladorA.ejecutar.(cargarPrograma suma10y22)) xtInfinito
+--se puede ver el resultado de 32 en el acumuladorA
+
+--No llega a tirar False ni True,ya que, se queda analizando infinitamente a la memoria del Micro, por lo tanto,
+--la consola no tira ningun resultado
+
+--Al tener una memoria infinita, si una funcion usa esa memoria dependiendo de si tenga que usar toda la memoria o no
+--esta podria quedarse infinitamente "analizando"
